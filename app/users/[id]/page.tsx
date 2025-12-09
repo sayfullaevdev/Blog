d3ae73b00b8cd8fd595e96c21044b28a234feda8
sayfullaevdev <sayfullaevdeve@gmail.com>
2025-12-09 13:17:14 +0500

    fix: page user

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,18 +1,28 @@
+import React from "react";
 interface PageProps {
-  params: {
-    id: string;
-  };
+	params: {
+		id: string;
+	};
 }
 
-export default async function Page({ params }: PageProps) {
-  const id = params.id;
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-  const user = await res.json();
+const page: React.FC<PageProps> = async ({ params }) => {
+	const { id } = params;
+	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+		next: { revalidate: 3600 },
+	});
 
-  return (
-    <div>
-      <h1>Users page</h1>
-      <h2>{user.name}</h2>
-    </div>
-  );
-}
+	if (!res.ok) {
+		return <div>Error: Failed to fetch user data.</div>;
+	}
+
+	const user = await res.json();
+	return (
+		<div>
+			<h1>User Detail Page</h1>
+			{user.name ? (<h2>{user.name}</h2>) : (<h2>User not found.</h2>)}
+			<p>ID: {id}</p>
+		</div>
+	);
+};
+
+export default page;
